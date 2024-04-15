d49cbc12c14681bab89a8858642671c82498f06d
Ammouchi abdallah <bily-f@hotmail.fr>
2024-04-16 00:48:12 +0200

    fin du module statistique

--- src/Controller/StatistiqueController.php
+++ src/Controller/StatistiqueController.php
@@ -17,6 +17,15 @@ use Symfony\UX\Chartjs\Model\Chart;
 
 class StatistiqueController extends AbstractController
 {    
+  //Contrller pour afficher la page statistique
+  #[Route("/statistiques", name:"statistiques")]
+  public function index():Response{
+    
+    return $this->render('statistique/index.html.twig',[
+
+    ]);
+  }
+
 
     //Controller pour afficher le chiffre d'affaire par année et par mois
     #[Route('/chiffreAffaire', name: 'chiffreAffaire')]
@@ -27,13 +36,13 @@ class StatistiqueController extends AbstractController
         $chiffreAffaire = $commandeRepo->createQueryBuilder('c')
             ->select('MONTH(c.date_commande) AS mois', 'SUM(c.total_commande) AS chiffreAffaire')
             ->where('YEAR(c.date_commande) = :annee')
-            ->setParameter('annee', $anneeSelectionnee)
+            ->setParameter('annee', $anneeSelectionnee ?: date("Y"))
             ->groupBy('mois')
             ->getQuery()
             ->getResult();
           // dd($chiffreAffaire ) ;
 
-        return $this->render('statistique/index.html.twig', [
+        return $this->render('statistique/caMois.html.twig', [
             'anneeSelectionnee' => $anneeSelectionnee,
             'chiffreAffaire' => json_encode($chiffreAffaire),
             'now' => new \DateTime()
@@ -43,40 +52,36 @@ class StatistiqueController extends AbstractController
 #[Route('/caFournisseur', name: 'caFournisseur')]
 public function recetteParFournisseur(LigneCommandeRepository $ligne, FournisseurRepository $fr, Request $request): Response
 {  
-    //Récupérer la liste des fournisseur
-    $fournisseurs=$fr->findAll();
-   //Tableau pour stocker les chiffres d'affaires par fournisseur
-   $chiffreAffaireParFournisseur=[];
-    //Récupérer l'année sélectionnée par l'admin dans le formulaire de twig
-    $anneeSelectionnee = $request->query->get('annee'); 
-
-    // Calculer le chiffre d'affaire par chaque fournisseur  pour une année sélectionnée
-
-    foreach ($fournisseurs as $fournisseur) { 
-        $chiffreAffaire = $ligne->createQueryBuilder('f')
-    ->select('f', 'SUM(lc.quantite * lc.prix_vente) AS ca_fournisseur')
-    ->join('f.Produit', 'p')
-    ->join('p.ligneCommandes', 'lc')
-    ->join('lc.Commande', 'c')
-    ->where('f.id = :fournisseurId')
-    ->andWhere('Year(c.date_commande) = :anneeSelectionnee')
-    ->setParameter('fournisseurId', $fournisseur->getId())
-    ->setParameter('anneeSelectionnee', $anneeSelectionnee)
-    ->getQuery()
-    ->getResult();
-    $chiffreAffaireParFournisseur[$fournisseur->getNomEntreprise()]=$chiffreAffaire;
-    }
-    $nomFournisseurs=array_keys($chiffreAffaireParFournisseur);
-    $chiffreAffaires=array_values($chiffreAffaireParFournisseur);
+     //Récupérer la liste des fournisseurs
+     $fournisseurs = $fr->findAll();
+     //Tableaux pour stocker les noms de fournisseurs et les chiffres d'affaires
+     $nomFournisseurs = [];
+     $chiffreAffaires = [];
    
-
-    return $this->render('statistique/caFournisseur.html.twig', [
-        'anneeSelectionnee' => $anneeSelectionnee,
-        'chiffreAffaireParFournisseur' => json_encode($chiffreAffaireParFournisseur),
-        'nomFournisseurs'=>json_encode($nomFournisseurs),
-        'chiffreAffaires'=>json_encode($chiffreAffaires),
-        'now' => new \DateTime()
-    ]);
+     //Récupérer l'année sélectionnée par l'admin dans le formulaire de twig
+     $anneeSelectionnee = $request->query->get('annee'); 
+ 
+     // Calculer le chiffre d'affaire par chaque fournisseur pour une année sélectionnée
+     foreach ($fournisseurs as $fournisseur) { 
+         $CA = $ligne->createQueryBuilder('f')
+             ->select('SUM(lc.quantite * lc.prix_vente) AS ca_fournisseur')
+             ->join('f.Produit', 'p')
+             ->join('p.ligneCommandes', 'lc')
+             ->join('lc.Commande', 'c')
+             ->where('f.id = :fournisseurId')
+             ->andWhere('Year(c.date_commande) = :anneeSelectionnee')
+             ->setParameter('fournisseurId', $fournisseur->getId())
+             ->setParameter('anneeSelectionnee', $anneeSelectionnee ?: date("Y")) // par défaut $anneeSelectionnee l'année en cours
+             ->getQuery()
+             ->getSingleScalarResult();
+         $nomFournisseurs[] = $fournisseur->getNomEntreprise();
+         $chiffreAffaires[] = $CA;
+     }
+     return $this->render('statistique/caFournisseur.html.twig', [
+         'anneeSelectionnee' =>  $anneeSelectionnee,
+         'nomFournisseurs' => json_encode($nomFournisseurs),
+         'chiffreAffaires' => json_encode($chiffreAffaires),
+         'now' => new \DateTime()
+     ]);
 }
-
 }
