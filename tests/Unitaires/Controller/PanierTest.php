2df19fc6223a776a84e303898b8f8774b2c10557
ammouchi <bily-f@hotmail.fr>
2024-06-21 11:03:32 +0200

    modification test panther

--- tests/Unitaires/Controller/PanierTest.php
+++ tests/Unitaires/Controller/PanierTest.php
@@ -12,28 +12,48 @@ class PanierTest extends PantherTestCase
         $crawler = $client->request('GET', '/');
 
         sleep(2);
+
         // Sélectionner et cliquer sur une catégorie
         $link = $crawler->selectLink("Séjour")->link();
         $crawler = $client->click($link);
 
         sleep(2);
 
-       //Choisir un produit et l'ajouter au panier
-       
+        // Choisir un produit et l'ajouter au panier
         $link = $crawler->selectLink("Ajouter au panier")->link();
         $crawler = $client->click($link);
 
-        // Attendre que le bouton valider mon panier s'affiche 
-        // $client->waitFor('.btn.btn-primary.mt-3');
         sleep(2);
-        
-        // clicker pour valider le panier
-        
-        $link=$crawler->selectLink('Valider mon panier')->link();
+
+        // Attendre que le bouton "Valider mon panier" s'affiche et cliquer
+        $client->waitFor('.btn.btn-primary.mt-3');
+        $link = $crawler->selectLink('Valider mon panier')->link();
         $crawler = $client->click($link);
 
-        $this->assertSelectorTextContains('h2','Connexion à mon compte');
-        
+        sleep(2);
+
+        // Se connecter pour valider la commande
+        $form = $crawler->selectButton('Me connecter')->form();
+        $form['email'] = 'bily-f@hotmail.fr';
+        $form['password'] = '123456';
+        $crawler = $client->submit($form);
+
+        sleep(5);
+   
+
+        // Remplir et valider le formulaire de la commande
+        $form = $crawler->selectButton('Payer ma commande')->form();
+        $form['adresse_livraison'] = '13 rue Charles August';
+        $form['cp_livraison'] = '80000';
+        $form['ville_livraison'] = 'Amiens';
+        $form['adresse_facturation'] = '13 rue Charles August';
+        $form['cp_facturation'] = '80000';
+        $form['ville_facturation'] = 'Amiens';
+        $crawler = $client->submit($form);
+
+        // Vérifier que la page de connexion s'affiche
+        $this->assertSelectorTextContains('h2', 'Connexion à mon compte');
+
         sleep(2);
     }
 }
